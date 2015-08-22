b48f8eb11489c7242dcf7b4ea990e2e48d57032e
CyMadigan <CyMadigan@hotmail.com>
2015-08-22 22:56:37 +0100

    Removed some unused sections

--- Pluralsight/Introduction to Versioning Environments with Vagrant/ubuntu/nginx/Provisioning/Scripts/provision.sh
+++ Pluralsight/Introduction to Versioning Environments with Vagrant/ubuntu/nginx/Provisioning/Scripts/provision.sh
@@ -5,15 +5,4 @@ apt-get -y install nginx
 sudo rm -rf /etc/nginx/sites-enabled
 sudo cp -r /vagrant/Projects/nginx/sites-enabled /etc/nginx
 
-# wget -qO- localhost
-# ls /etc/nginx/sites-enabled/
-# head -30 /etc/nginx/sites-enabled/default
-# ls /usr/share/nginx/www
-# cat /usr/share/nginx/www/index.html
-# ls /vagrant
-# cp -r /usr/share/nginx/www /vagrant/www
-
-# sudo rm -rf /usr/share/nginx/www
-# sudo ln -s /vagrant/Projects/nginx/www /usr/share/nginx/www
-
 service nginx start
