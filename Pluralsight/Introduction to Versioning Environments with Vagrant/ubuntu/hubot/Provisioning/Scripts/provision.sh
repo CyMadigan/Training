ac426395f27636d614f81f6ae0df5e62b1a8f585
CyMadigan <CyMadigan@hotmail.com>
2015-08-23 18:06:44 +0100

    Create hubot dev environment with ubuntu trusty

---
+++ Pluralsight/Introduction to Versioning Environments with Vagrant/ubuntu/hubot/Provisioning/Scripts/provision.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cp /vagrant/Provisioning/Scripts/.bashrc .bashrc
+
+apt-get -y update
+
+# check if npm package already installed; if not then we install nodejs and npm
+#   sets the symbolic link
+dpkg -s npm &>/dev/null || {
+  apt-get -y install nodejs npm
+  ln -s /usr/bin/nodejs /usr/bin/node
+}
+
+# check if hubot package already installed; if not then we install hubot and cofeescript
+#   sets the symbolic link
+command -v hubot &>/dev/null || {
+  npm install -g hubot coffee-script
+}
